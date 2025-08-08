0f2a980d7274f6fd2fba8adca87ed144e1458ba0
Eldar Khaitov <eldar.khaitov@gmail.com>
2025-08-08 12:01:12 +0200

    Update Google Auth flow and check endpoint

--- src/iam/authentication/guards/authentication/authentication.guard.ts
+++ src/iam/authentication/guards/authentication/authentication.guard.ts
@@ -1,13 +1,27 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+} from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { IS_PUBLIC_KEY } from "src/common/decorators/public.decorator";
 import { AccessTokenGuard } from "../access-token/access-token.guard";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+import { ConfigType } from "@nestjs/config";
+import jwtConfig from "../../../config/jwt.config";
+import { UserPayload } from "../../../types/UserPayload";
+import { REQUEST_USER_KEY } from "../../../iam.constants";
 
 @Injectable()
 export class AuthenticationGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
     private readonly accessTokenGuard: AccessTokenGuard,
+    private readonly jwtService: JwtService,
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
@@ -16,8 +30,15 @@ export class AuthenticationGuard implements CanActivate {
       context.getHandler(),
       context.getClass(),
     ]);
+    const request: Request = context.switchToHttp().getRequest();
 
     if (isPublic) {
+      if (request.cookies.accessToken) {
+        const user: UserPayload = this.jwtService.decode(
+          request.cookies.accessToken as string,
+        );
+        request[REQUEST_USER_KEY] = user;
+      }
       return true;
     }
 
